9ac092c14e15c7f095744e2fd063a51268fd1632
Sushkow Andrey <andreysushkow@mail.ru>
2021-05-08 21:48:28 +0300

    Библиотека dll

---
+++ CheckPasword/CheckPasword/ProccessCheckPassword.cs
@@ -0,0 +1,25 @@
+﻿namespace CheckPasword
+{
+    public delegate void InformationCheck(string message);
+    public delegate bool UserCheck(string login, string password);
+    public class ProccessCheckPassword
+    {
+        public event InformationCheck Success;              // 1.Определение события
+        public event InformationCheck Error;
+        public ProccessCheckPassword() {}
+        public bool CheckPassword(UserCheck userCheck, string login, string password)
+        {
+            bool resultCheck = userCheck(login, password);
+            if (resultCheck)
+            {
+                Success?.Invoke($"Пользователь с логином: {login} авторизован. Вход разрешен");   // 2.Вызов события
+            }
+            else if (!resultCheck)
+            {
+                Error?.Invoke($"Ошибка логина {login} или пароляю Вход запрещен"); // 2.Вызов события
+            }
+            return resultCheck;
+        }
+
+    }
+}
